46268aad1112bec2dd1ce0657149e078c2e980f9
Rafael Dias <rafa197795@gmail.com>
2020-12-01 11:39:29 -0300

    tests(day01): create tests for part 1 and 2
    Signed-off-by: Rafael Dias <rafa197795@gmail.com>

--- Day01.playground/Contents.swift
+++ Day01.playground/Contents.swift
@@ -1,4 +1,5 @@
 import Cocoa
+import XCTest
 
 // Read input from file
 var filePath = Bundle.main.path(forResource: "input", ofType: "txt")!
@@ -9,32 +10,56 @@ guard let rawInput = content?.split(separator: "\n") else { exit(-1) }
 let input = rawInput.map { Int($0) ?? -1 }
 
 // MARK:- Part 1
-var answer: Int = 0
+func partOne(input: [Int]) -> Int {
+    var answer: Int = 0
 
-for (index, value) in input.enumerated() {
-    for newIndex in index..<input.count {
-        if value + input[newIndex] == 2020 {
-            answer = value * input[newIndex]
-            break
-        }
-    }
-}
-
-print(answer)
-
-// MARK:- Part 2
-answer = 0
-for (index, value) in input.enumerated() {
-    var tmp = input
-    tmp.remove(at: index)
-    for (i, v) in tmp.enumerated() {
-        for newI in i..<tmp.count {
-            if value + v + tmp[newI] == 2020 {
-                answer = value * v * tmp[newI]
+    for (index, value) in input.enumerated() {
+        for newIndex in index..<input.count {
+            if value + input[newIndex] == 2020 {
+                answer = value * input[newIndex]
                 break
             }
         }
     }
+
+    return answer
 }
 
-print(answer)
+print(partOne(input: input))
+
+// MARK:- Part 2
+func partTwo(input: [Int]) -> Int {
+    var answer = 0
+    for (index, value) in input.enumerated() {
+        var tmp = input
+        tmp.remove(at: index)
+        for (i, v) in tmp.enumerated() {
+            for newI in i..<tmp.count {
+                if value + v + tmp[newI] == 2020 {
+                    answer = value * v * tmp[newI]
+                    break
+                }
+            }
+        }
+    }
+
+    return answer
+}
+
+
+print(partTwo(input: input))
+
+
+class Day01Tests: XCTestCase {
+    func testPartOne() {
+        var answer = 494475
+        XCTAssertEqual(answer, partOne(input: input))
+    }
+
+    func testPartTwo() {
+        var answer = 267520550
+        XCTAssertEqual(answer, partTwo(input: input))
+    }
+}
+
+Day01Tests.defaultTestSuite.run()
